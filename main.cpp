dcf431a2b6b65a01b3d602ef67192365c413d4d9
Luigi Leonardi <luigix25@gmail.com>
2022-04-22 17:09:28 +0200

    Cleanup of main

--- main.cpp
+++ main.cpp
@@ -1,52 +1,13 @@
 #include <iostream>
 #include <fstream>
-#include <queue>
-#include <vector>
-#include <unordered_map>
-
 #include <bitset>
 #include <filesystem>
 
-using namespace std;
+#include "Encode.h"
 
-#define TIPO_1 char
-#define TIPO_2 uint16_t
-
-struct leaf{
-    TIPO_1 key;
-    TIPO_2 value;
-    leaf *left, *right;
-    leaf(){}
-    leaf(TIPO_1 key, TIPO_2 value) : key(key),value(value),left(NULL), right(NULL){}
-    leaf(TIPO_1 key, TIPO_2 value, leaf *left, leaf *right) : key(key),value(value),left(left), right(right){}
-
-};
-
-class Compare
-{
-public:
-    bool operator() (leaf *uno, leaf *due)
-    {
-        if(uno->value > due->value)
-            return true;
-        return false;
-    }
-};
-
-
-
-typedef priority_queue<leaf*,vector<leaf*>,Compare> my_queue;
-typedef unordered_map<TIPO_1,TIPO_2> my_map;
-
-typedef unordered_map<char,string> huffman_map;
-typedef unordered_map<string,char> huffman_map_inverted;
 
 void print_queue(my_queue coda);
 string decode(const huffman_map &code,const string &encoded);
-
-my_queue count_occurencies(string str);
-huffman_map huffmann_code(my_queue occorrenze);
-
 
 int main(){
 
@@ -60,22 +21,14 @@ int main(){
     getline (file,line);
     file.close();
 
+    Encode enc(line);
+    string encoded = enc.getEncoding();
+
     ofstream out("encoded.txt",ios::binary);
 
-    my_queue occorrenze = count_occurencies(line);
-    huffman_map codice = huffmann_code(occorrenze); 
-    
-    /*for (auto pair: codice) {
-		cout << pair.first << " " << pair.second << '\n';
-	}*/
-
-    string encoded;
+   
     uint8_t to_write;
     
-    for(char c: line){
-        encoded += codice[c];
-    }
-
     uint32_t index = 0;
     const uint32_t str_length = encoded.length();
 
@@ -110,11 +63,14 @@ int main(){
         encoded += bit.to_string();   
     }
 
-    string decoded = decode(codice,encoded);
+    string decoded = decode(enc.getHuffmanCode(),encoded);
 
     cout<<decoded<<endl;
 
 }
+
+
+
 
 string decode(const huffman_map &code,const string &encoded){
     
@@ -154,68 +110,7 @@ string decode(const huffman_map &code,const string &encoded){
    return decoded;
 }
 
-void visit_tree(leaf *node, string code, huffman_map &codes){
-    if(node == NULL)
-        return;
 
-    if(node->left == NULL && node->right == NULL){
-        codes[node->key] = code;
-        return;
-    }
-
-    visit_tree(node->left,code +"0",codes);
-    visit_tree(node->right,code +"1",codes);
-
-}
-
-huffman_map huffmann_code(my_queue occorrenze){
-
-    while(occorrenze.size() > 1){
-        leaf *uno = occorrenze.top();
-        occorrenze.pop();
-        leaf *due = occorrenze.top();
-        occorrenze.pop();
-
-        leaf *parent = new leaf;
-
-        parent->left = uno;
-        parent->right = due;
-        parent->value = uno->value + due->value;
-
-        occorrenze.push(parent);
-    }
-
-    leaf *root = occorrenze.top();
-    string empty;
-
-    huffman_map return_map;
-
-    visit_tree(root,empty,return_map);
-
-    return return_map;
-
-}
-
-
-my_queue count_occurencies(string str){
-
-    my_map dictionary(21); //init to 0
-
-    for (const char &c: str) {
-        dictionary[c]++;
-    }
-
-    //TODO: sta roba fa trashing
-    my_queue sorted;
-
-    for (auto &e: dictionary) {
-        leaf *f = new leaf(e.first,e.second);
-        sorted.push(f);
-    }
-
-    return sorted;
-
-}
 
 void print_queue(my_queue sorted){
 
